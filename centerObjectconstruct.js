a518e5363a8f2f120600aafe802979cddb43d287
Søren Nørup <soeren96@hotmail.com>
2016-01-08 13:38:58 +0100

    Dropdown function

---
+++ centerObjectconstruct.js
@@ -0,0 +1,79 @@
+
+  //uucenter object 
+ var size;
+var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+var pinColor = "336699";
+ 
+ function uuCenter(centerName,leader,adress,lat,lng,telefon,email,url,covers,space,marker){
+this.centerName=centerName;
+this.leader=leader;
+this.adress=adress;
+
+this.lat=lat;
+this.lng=lng;
+this.telefon=telefon;
+this.email=email;
+this.url=url;
+this.covers = covers;
+this.space=
+function(){
+   var pos=new google.maps.LatLng(this.lat,this.lng);
+    return pos;
+}
+/*this.popUp=
+function(){
+    
+    google.maps.event.addListener(mark, "mouseover", function() {
+         infoW.open(map,this.marker());
+    });
+     return infoW;
+}*/
+this.marker=
+function(map){
+    var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|"+pinColor);
+    var mark=new google.maps.Marker({
+        icon: pinImage,
+        position:this.space(),
+        url:this.url,
+    
+
+    })
+    mark.setAnimation();
+     /*google.maps.event.addListener(mark, "click", function() {
+       window.location.href = this.url;
+        
+      });  */
+   var infoW = new google.maps.InfoWindow({
+         maxWidth:500,
+       
+        content:" <div style='width:auto;height:100px;font-family:calibri;z-index:-1;'> <b>Centernavn:</b> "+this.centerName+"<br> "+"<b>Centerleder:</b> "+this.leader+"<br/> "+"<b>Adresse:</b> "+this.adress
+        +"<br/>"+"<b>Dækker:</b> "+this.covers +"<br/> "+"<b>Telefon:</b> "+this.telefon+"<br/> "+"<b>Email:</b> "+this.email+"<br/> "+"<b>Hjemmeside:</b> <a href="+this.url+">"+this.url+"</a>"+"<br/> </div>"
+       
+});
+       google.maps.event.addListener(mark, "click", function() {
+            
+           infoW.open(map,mark);
+          
+          
+           
+           
+    });
+     
+     
+       /*google.maps.event.addListener(mark,"click",function(){
+       
+           infoW.close();
+           count= 0 ;
+          
+         
+       });*/
+     google.maps.event.addListener(mark,"click",function(){
+   
+           //window.location.href=this.url;
+         
+        });
+    return mark
+}
+
+
+ }
--- danmarkskort.php
+++ danmarkskort.php
@@ -1,4 +1,4 @@
-<?php include "stamkort_data.php";?>
+﻿<?php include "stamkort_data.php";?>
 
 <script type="text/javascript">var jArray= <?php echo json_encode($exValues ); ?>;
 function printInfo(a){
@@ -16,22 +16,37 @@ function printInfo(a){
 <script type="text/javascript" src="centerObjectconstruct.js"></script>
 <script type="text/javascript">
   
-  /*var zoom= 8;
-  var b = "bob";
-  if (b=="bob") {
-    var center=new google.maps.LatLng(56.266427,10.292759);
-    var zoom = 7;
-  }
- else{
-  var center=new google.maps.LatLng(55.6739462, 12.562308700000017);
-  var zoom = 8;
- }*/
 
      google.maps.event.addDomListener(window, 'load', initialize);
+     
+     
+     function createCenterObject(){
+       var centerObject=new Array();
+            
+ for (var i=0;i<jArray.length;i++) {                  
+          centerObject[i]=new uuCenter(
+          
+          jArray[i][0],
+          jArray[i][1],
+          jArray[i][2],
+          jArray[i][3],
+          jArray[i][4],
+          jArray[i][5],
+          jArray[i][6],
+          jArray[i][7],
+          jArray[i][8],
+          jArray[i][9],
+          jArray[i][10] 
+                                           );       
+           }     
+      return centerObject;
+      
+     }
+     
 
  function initialize(zoom,place)
 {
-   
+  
   var place = place;
   if (place=="1") {
     var center=new google.maps.LatLng(56.266427,10.292759);
@@ -49,6 +64,18 @@ function printInfo(a){
   var center=new google.maps.LatLng(55.472174, 9.134411);
   var zoom = zoom;
  }
+ else if (place = "5"){
+   var latLng = onIndexClicked();
+   splitStr = latLng.split(",");
+   var center =new google.maps.LatLng(parseFloat(splitStr[0]),parseFloat(splitStr[1]));
+    var zoom = zoom;
+    
+  
+ 
+ 
+ }
+ 
+ 
  else{
   var center=new google.maps.LatLng(56.266427,10.292759);
  }
@@ -87,38 +114,26 @@ map.setOptions({zoomControl:true, scrollwheel: false ,navigationControl: false,
     }  
     );
   
-   var centerObject=new Array();
-            
- for (var i=0;i<jArray.length;i++) {                  
-          centerObject[i]=new uuCenter(
-          
-          jArray[i][0],
-          jArray[i][1],
-          jArray[i][2],
-          jArray[i][3],
-          jArray[i][4],
-          jArray[i][5],
-          jArray[i][6],
-          jArray[i][7],
-          jArray[i][8],
-          jArray[i][9],
-          jArray[i][10] 
-                                           );       
-           }     
+ 
+  var obj = createCenterObject();
+ 
  for(var i=0;i<jArray.length;i++){
-          centerObject[i].marker(map).setMap(map);
+          obj[i].marker(map).setMap(map);
+           
+            }         
+  
+              center.marker.setMap(map);
+        
 
-            }          
-              center.marker.setMap(map); 
   
 }   
 </script>
 
 </head>
-<body onload="initialize(7,'1')">
-     
+<body onload="initialize(7,'1')"> 
+          
 
- 
+  
   <button onclick="initialize(7,'1')">Danmark</button>
    <button onclick="initialize(8,'2')">Sjælland/Hovedstaden</button>
     <button onclick="initialize(8,'3')">Nord og MidtJylland</button>
@@ -126,17 +141,32 @@ map.setOptions({zoomControl:true, scrollwheel: false ,navigationControl: false,
     
   
     
-    <!--<a style="color:#336699;font-family:calibri;position: absolute;top:50px;left:25%;z-index:1;" href="http://uudanmark.dk">Tilbage til forsiden</a>
-    <a style="color:#336699;font-family:calibri;position: absolute;top:50px;left:40%;z-index:1;" href="http://uudanmark.dk/wp-content/uploads/2014/03/Liste-over-UU-centre-til-hjemmesiden_4_Marts_-2014.pdf">UU_CENTRE 20. Feb 2014 </a> <br/><a style="color:#336699;font-family:calibri;position: absolute;top:50px;left:55%;z-index:1;" href="mailto:uudk@uudanmark.dk"> (Rettelser sendes til: uudk@uudanmark.dk)</a>
-     
-    <p style="position: absolute;top:100px;left:25%;z-index:1;"><img src="uudk_logo.png" width="250"/></p>
-    <p style="color:#336699; position: absolute; top:60px; left:25%;z-index: 1;font-family:calibri;font-style:normal; font-size:30px;">UU-centre 2014</h3>
-    
-    <!--   <img style="position: absolute; z-index:1; right:200px; width:500px;height:200px;"src="cover.png" />-->
+   <select id="names" onclick="initialize(20,'5')"></select>
 <div id="googleMap" style="width:100%;height:600px; "> </div>
- <!--<p style="border:3px solid;border-color: #336699;position: absolute;right:45%;z-index: 2;top:-10px;"><a href="google_map2.php"><img src="bornholm.png" width="100"></a></p>-->
 
+<script>
 
+var obj2 = createCenterObject();
+for (var i=0; i<obj2.length; i++) {
+    document.getElementById("names").options[i] = new Option(obj2[i].centerName, obj2[i].lat+","+obj2[i].lng);
+   
+}
+function onIndexClicked(){
+ 
+  var elementId =  document.getElementById("names");
+ var space = elementId.options[elementId.selectedIndex].value;
+  return space;
+  
+}
+function zoomToPosition(){
+ 
+  
+}
+ 
+</script>
+
+     
+         
 </body>
 <script type=text/javascript>
   
@@ -144,5 +174,10 @@ map.setOptions({zoomControl:true, scrollwheel: false ,navigationControl: false,
     alert(id);
     
    }
+   
+  
+
+
+     }
 </script>
 </html>
